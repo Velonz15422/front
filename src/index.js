f22dbe1946c95e7d3d5aa1f8e03d4eca42864e72
velonz <santiago15422@gmail.com>
2022-10-25 10:24:49 -0500

    Uso de state provider en el index con su estado inicial

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StateProvider } from './StateProvider';
+import reducer, {initialState} from './Reducer';
+import './firebase'
+import 'bootswatch/dist/quartz/bootstrap.min.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+    
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start meanpm startsuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
