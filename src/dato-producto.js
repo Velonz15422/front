21e4d866dad25a1cae441fd7783311c7e026e015
velonz <santiago15422@gmail.com>
2022-10-24 20:26:41 -0500

    Estructura de Productos

---
+++ src/dato-producto.js
@@ -0,0 +1,31 @@
+const productos =[
+    {
+        id: 1,
+        nombre: "Bravecto",
+        tipo: "Medicamento",
+        precio: 30000,
+        rating: 1,
+        imagen: "https://animalsveterinaria.vteximg.com.br/arquivos/ids/159909-450-450/VALBRA003.jpg?v=637630055589500000",
+        descripcion:"Es como algo pa la rabia" 
+
+    },
+    {
+        id: 2,
+        nombre: "NexGard",
+        tipo: "Medicamento",
+        precio: 20000,
+        rating: 5,
+        imagen: "https://animalsveterinaria.vteximg.com.br/arquivos/ids/156832-450-450/MERNEX001.jpg?v=637229544989830000",
+        descripcion: "Un medicamento ahi que ni idea "
+
+    },
+    {
+        id: 3,
+        nombre: "Cardial",
+        tipo: "Medicamento",
+        precio: 120000,
+        rating: 4,
+        imagen: "https://animalscenter.com/1563-home_default/cardial-5-mg-caja-30-comprimidos.jpg",
+        descripcion: "Como pal corazon"
+
+    },
