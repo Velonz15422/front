349901a3e602a4a6a20d17be4bc404cec3d81df8
velonz <santiago15422@gmail.com>
2022-10-25 08:55:01 -0500

    Rutas de enlace en la carpeta app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Productos from './components/Productos';
+import PaginaCheckout from './components/PaginaCheckout'
+import { Routes, BrowserRouter as Router, Route, BrowserRouter } from "react-router-dom"
+import InicioSesion from './components/InicioSesion'
+import SignUp from './components/SignUp'
+import { useEffect } from 'react';
+import { auth } from './firebase';
+import { ActionTypes } from '@mui/base';
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{ user }, dispatch] = useStateValue()
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log(authUser);
+      if (authUser) {
+        dispatch({
+          type: ActionTypes.SET_USER,
+          user: authUser,
+        })
+      }
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Productos />} />
+      </Routes>
+      <Routes>
+        <Route path='/login' element={<InicioSesion />} />
+      </Routes>
+      <Routes>
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+      <Routes>
+        <Route path='/pagina-pago' element={<PaginaCheckout />} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
